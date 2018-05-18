90fb48c14172734c3a5a2c1cf03349ee895ea5b9
Anthony <anthony@anthonys-mbp.lan>
2018-05-18 14:33:34 -0400

    Changes

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,32 +1,36 @@
 const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
 
-const createUser = (req, res) => {
-  const { username, password } = req.body;
-  // create user takes in the username and password and saves a user.
-  // our pre save hook should kick in here saving this user to the DB with an encrypted password.
-  User
-  .findOne({ username })
-  .then(user => {
-    if(user) {
-      user.checkPassword(plainTextPW).then(validPassword => {
-        if(validPassword) {
-          req.session.username = user.username;
-          req.send('have a cookie!');
-        }else {
-          res.status(401);
-          res.send('Invalid Login Credentials');
-        }
-      })
-    } else {
-      res.status(401);
-      res.send('Invalid Login Credentials')
-    }
-  })
-  .catch(err => {
-    res.send(err);
-  })
-};
+server.post('/login', (req, res) => {
+  const createUser = (req, res) => {
+    const { username, password } = req.body;
+    // create user takes in the username and password and saves a user.
+    // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+    User
+    .findOne({ username })
+    .then(user => {
+      if(user) {
+        user.checkPassword(plainTextPW).then(validPassword => {
+          if(validPassword) {
+            req.session.username = user.username;
+            req.send('have a cookie!');
+            //add cookie token
+          }else {
+            res.status(401);
+            res.send('Invalid Login Credentials');
+          }
+        })
+      } else {
+        res.status(401);
+        res.send('Invalid Login Credentials')
+      }
+    })
+    .catch(err => {
+      res.send(err);
+    })
+  };
+})
+
 
 module.exports = {
   createUser
